1b7559ddacdabe4b0454081f1c0576e907097edc
Youssef-Douki <youssefdoukii1@gmail.com>
2022-02-10 18:09:05 +0100

    problems fixed

--- server/controllers/product.controler.admin.js
+++ server/controllers/product.controler.admin.js
@@ -45,5 +45,27 @@ var login =(req,res)=>{
     })
 }
 
-
-module.exports={getALL,signUp,login}
+addRestaurant=(req,res)=>{
+    var params=[req.body.name,req.body.picture,req.body.description]
+esm = req.body.name
+var str="INSERT INTO restaurant (name , picture , description) VALUES (?,?,?)"
+db.query(str,params,(err,result)=>{
+    err?console.log(err):res.status(200).send("restaurant cbon mawjoud")
+})
+}
+////////////////// menu  for
+addMenu=(req,res)=>{
+    var params=[req.body.food_name,req.body.price]
+    str=`insert into menu (food_name,price,restaurant_id) VALUES (?,?,(SELECT  restaurant_id from restaurant where name="${esm}"))`
+    db.query(str,params,(err,result)=>{
+        err?console.log(err):res.status(200).send("menu rtzad")
+    })
+}
+ ////////// get all menu for on restaurant //:
+ getMenuOneRestaurant=(req,res)=>{
+     var str=`select * from menu where restaurant_id =(select restaurant_id from restaurant where name = "${esm}")`
+     db.query(str,(err,result)=>{
+         err?console.log(err):res.send(result)
+     })
+ }
+module.exports={getALL,signUp,login,addRestaurant,addMenu,getMenuOneRestaurant}
--- server/database_mysql/schema.sql
+++ server/database_mysql/schema.sql
@@ -1,7 +1,5 @@
 DROP DATABASE IF EXISTS delivery;
-
 CREATE DATABASE delivery;
-
 USE delivery;
 CREATE TABLE user (
   id int NOT NULL AUTO_INCREMENT,
@@ -12,20 +10,23 @@ CREATE TABLE user (
   phoneNumber int ,
   points int ,
   profilePicture varchar(255),
-
-  
-  PRIMARY KEY (ID)
+  PRIMARY KEY (id)
 );
 CREATE TABLE restaurant (
-  id int NOT NULL AUTO_INCREMENT,
-  name varchar(50),     
+  restaurant_id int NOT NULL AUTO_INCREMENT,
+  name varchar(50),    
   password varchar(64),
   picture  varchar(255),
-  description varchar(255),
-    menu varchar(255),
-  
-  PRIMARY KEY (ID)
+  description varchar(255)
+  PRIMARY KEY (restaurant_id)
+);
+CREATE TABLE menu (
+  id int NOT NULL AUTO_INCREMENT,
+  food_name varchar(50),
+  price int ,
+  restaurant_id int,
+  PRIMARY KEY(id),
+  FOREIGN KEY (restaurant_id) REFERENCES restaurant(restaurant_id)  
 );
 /* excute schema  */
 /* mysql -u root -p <server/database_mysql/schema.sql */
-
